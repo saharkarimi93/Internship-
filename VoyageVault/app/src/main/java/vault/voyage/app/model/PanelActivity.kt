88c814b168d5f9795008531552c6892055d7dbd6
saharkarimi93 <sahar.karimi8927@gmail.com>
2023-05-13 18:22:51 -0500

    UpdatedPanelActivity

---
+++ VoyageVault/app/src/main/java/vault/voyage/app/model/PanelActivity.kt
@@ -0,0 +1,12 @@
+package vault.voyage.app.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vault.voyage.app.R
+
+class PanelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.panel_activity)
+    }
+}
