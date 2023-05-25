f7db0da1f6528c2f17d7a0135e5acd778811883d
saharkarimi93 <sahar.karimi8927@gmail.com>
2023-05-24 20:44:10 -0500

    updatedTodo

---
+++ VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/todoFragment/recyclerview/TodoItem.kt
@@ -0,0 +1,4 @@
+package vault.voyage.app.panelFragments.todoFragment.recyclerview
+
+class TodoItem(val description:String,var done:Boolean) {
+}
---
+++ VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/todoFragment/recyclerview/TodoItemsAdapter.kt
@@ -0,0 +1,21 @@
+package vault.voyage.app.panelFragments.todoFragment.recyclerview
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import vault.voyage.app.panelFragments.bagFragment.recyclerview.BagItem
+import vault.voyage.app.panelFragments.bagFragment.recyclerview.BagItemsAdapter
+
+class TodoItemsAdapter (val context: Context?, val items:List<TodoItem>): RecyclerView.Adapter<BagItemsAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BagItemsAdapter.Holder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BagItemsAdapter.Holder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
