d1a15508d66bf9362067061f5d7091306dad7b3a
saharkarimi93 <sahar.karimi8927@gmail.com>
2023-05-26 18:37:09 -0500

    updated

--- VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/todoFragment/recyclerview/TodoItemsAdapter.kt
+++ VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/todoFragment/recyclerview/TodoItemsAdapter.kt
@@ -1,21 +1,38 @@
 package vault.voyage.app.panelFragments.todoFragment.recyclerview
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import vault.voyage.app.R
 import vault.voyage.app.panelFragments.bagFragment.recyclerview.BagItem
 import vault.voyage.app.panelFragments.bagFragment.recyclerview.BagItemsAdapter
 
-class TodoItemsAdapter (val context: Context?, val items:List<TodoItem>): RecyclerView.Adapter<BagItemsAdapter.Holder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BagItemsAdapter.Holder {
-        TODO("Not yet implemented")
+class TodoItemsAdapter (val context: Context?, val items:List<TodoItem>): RecyclerView.Adapter<TodoItemsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemsAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.todo_items,parent,false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.count()
     }
 
-    override fun onBindViewHolder(holder: BagItemsAdapter.Holder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: TodoItemsAdapter.ViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.desc.text = currentItem.description
+        holder.done_task.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                Toast.makeText(context,"Task is Done",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    inner class ViewHolder(todoItemView: View): RecyclerView.ViewHolder(todoItemView) {
+        val done_task = todoItemView.findViewById<CheckBox>(R.id.task_done_checkbox)
+        val desc = todoItemView.findViewById<TextView>(R.id.task_information)
     }
 }
