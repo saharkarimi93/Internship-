97a7400fdeaa713592a9ab7b01da90dc9357ce4e
saharkarimi93 <sahar.karimi8927@gmail.com>
2023-05-26 19:45:32 -0500

    updated

--- VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/todoFragment/TodoFragment.kt
+++ VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/todoFragment/TodoFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import vault.voyage.app.R
+import vault.voyage.app.panelFragments.todoFragment.recyclerview.TodoItem
+import vault.voyage.app.panelFragments.todoFragment.recyclerview.TodoItemsAdapter
 
 
 class TodoFragment : Fragment() {
@@ -19,7 +23,12 @@ class TodoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_todo, container, false)
+        val todoItems_recyclerView:RecyclerView = view.findViewById(R.id.todo_items_recyclerview)
+        todoItems_recyclerView.layoutManager = LinearLayoutManager(context)
+        val items:List<TodoItem> = arrayListOf(TodoItem("Test 1"),TodoItem("Test 2"),TodoItem("Test 3"))
+        todoItems_recyclerView.adapter = TodoItemsAdapter(context,items)
+        return view
     }
 
 }
