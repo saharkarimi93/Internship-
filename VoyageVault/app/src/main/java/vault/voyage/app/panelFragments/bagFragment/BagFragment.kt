308f967d229d4a9a883b7792cecef617469e35e6
saharkarimi93 <sahar.karimi8927@gmail.com>
2023-06-09 21:23:55 -0500

    updated

--- VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/bagFragment/BagFragment.kt
+++ VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/bagFragment/BagFragment.kt
@@ -5,17 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import vault.voyage.app.R
 import vault.voyage.app.model.User
-import vault.voyage.app.panelFragments.bagFragment.bagSelectedItem.BagSelectedItemFragment
 import vault.voyage.app.panelFragments.bagFragment.bagSelectedItem.SelectedItem
 import vault.voyage.app.panelFragments.bagFragment.recyclerview.BagItem
 import vault.voyage.app.panelFragments.bagFragment.recyclerview.BagItemsAdapter
+import kotlin.streams.toList
 
 
 class BagFragment(user: User) : Fragment() {
@@ -33,24 +32,51 @@ class BagFragment(user: User) : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_bag, container, false)
         val bag_recyclerview = view.findViewById<RecyclerView>(R.id.bagItems_recyclerview)
         bag_recyclerview.layoutManager = LinearLayoutManager(context)
-        //Test
+
+        val clothingList = stringToMutableList("Stockings,Underwear,Pajamas,T shirts,Casual Dress,Evening Dress,Shirt,Cardigan,Vest,Jacket,Skirt,Trousers,Jeans,Shorts,Suit,Coat,Rain Coat,Glove,Hat, Scarf,Bikini,Belt,Slipper,Sneakers,Casual Shoes,Heeled Shoes,Sports Wear")
+        val personalCareList = stringToMutableList("Tooth-brush,Tooth-passe,Floss,Mouthwash,Shaving Cream,Razor Blade, Soap,Fiber,Shampoo,Hair Conditioner,Brush,Comb,Hair Dryer,Curling Iron," +
+                "Hai moulder,Hair clip, Moisturizer,Lip Cream, Contact Lens,Perfume,Deodorant,Makeup Materials,Makeup Remover,Wet Wipes,Pad,Ear Stick,Cotton,Nail Polish,Nail Polish Remover,Tweezers,Nail Scissors,Nail Files,Suntan cream")
+        val babyNeedsList = stringToMutableList("Snap suit,outfit,jumpsuit,baby socks,baby hat,baby pajamas,Baby Bath Towel, Muslin,Blanket,Dribble Bibs, Breast Pump,Water Bottle,Storage Container, Food Spoon, Highchairs,Diaper,WetWipes,Baby Cotton, Baby Care Cover,Diaper,Stroller,Carrier,Toys")
+        val healthList = stringToMutableList("Aspirin,Drugs Used,Vitamins Used,Lens Solutions,First Aid,Pain Reliever,Fever Reducer,Diarrhea Stopper,Pain Relieve Spray")
+        val technologyList = stringToMutableList("Mobile Phone,Phone Cover,E-Book Reader,Camera,Camera Charger,Portable Speaker,IPad,Headphone,Laptop,Laptop charger,Mouse,Extension Cable,Battery,Power Bank,Mp3 Player")
+        val foodList =stringToMutableList("Snack,sandwich,Juice,Tea Bags,Coffee,Water,Thermos,Chips,Baby food")
+        val beachSuppliesList =stringToMutableList("Sea Glasses,Sea Bed,Suntan Cream,Beach Umbrella, Swim Fins,Beach Bag,Beach Towel,Beach Slippers,Sunbed,Snorkel,WaterProof Clock")
+        val carSuppliesList =stringToMutableList("Pump,Car jack,spare car key,accident record set,auto refrigerator, car cover, car charger, window sun shades")
+        val needsList = stringToMutableList("Backpack,Daily Bags,Laundry Bag,sewing kit,travel lock,luggage tag,magazine,sports equipment, important numbers")
+
+
+
+
+
+
+
+
         val items = arrayListOf<BagItem>(
-            BagItem(R.drawable.clothing,"Clothing", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")), R.color.bag_Clothing),
-            BagItem(R.drawable.personal_care,"Personal Care", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(217,242,255)),
-            BagItem(R.drawable.baby_needs,"Baby Needs", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(255,255,209)),
-            BagItem(R.drawable.health,"Health", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(208,255,209)),
-            BagItem(R.drawable.technology,"Technology", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(208,255,142)),
-            BagItem(R.drawable.food,"Food", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(252,228,149)),
-            BagItem(R.drawable.beach_supplies,"Beach Supplies", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(173,247,240)),
-            BagItem(R.drawable.car_supplies,"Car Supplies", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(217,255,240)),
-            BagItem(R.drawable.needs,"Needs", mutableListOf(SelectedItem("Item 1"),SelectedItem("Item 2"),SelectedItem("Item 3")),Color.rgb(216,247,216)),
+            BagItem(R.drawable.clothing,"Clothing", clothingList, R.color.bag_Clothing),
+            BagItem(R.drawable.personal_care,"Personal Care", personalCareList,Color.rgb(217,242,255)),
+            BagItem(R.drawable.baby_needs,"Baby Needs", babyNeedsList,Color.rgb(255,255,209)),
+            BagItem(R.drawable.health,"Health", healthList,Color.rgb(208,255,209)),
+            BagItem(R.drawable.technology,"Technology", technologyList,Color.rgb(208,255,142)),
+            BagItem(R.drawable.food,"Food", foodList,Color.rgb(252,228,149)),
+            BagItem(R.drawable.beach_supplies,"Beach Supplies", beachSuppliesList,Color.rgb(173,247,240)),
+            BagItem(R.drawable.car_supplies,"Car Supplies", carSuppliesList,Color.rgb(217,255,240)),
+            BagItem(R.drawable.needs,"Needs", needsList,Color.rgb(216,247,216)),
         )
         bag_recyclerview.adapter = BagItemsAdapter(context, activity as AppCompatActivity,items)
 
 
 
-
         return view;
+    }
+
+    fun stringToMutableList(inputString:String): MutableList<SelectedItem> {
+        return inputString
+            .split(",")
+            .stream()
+            .map { e-> SelectedItem(e)}
+            .toList()
+            .toMutableList()
+
     }
 
 
