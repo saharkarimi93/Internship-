bc404789a61211a3350f2baa3fa19b7f17984b8f
saharkarimi93 <sahar.karimi8927@gmail.com>
2023-06-28 20:33:32 -0500

    NearMeFragment.ktModified

--- VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/nearmeFragment/NearMeFragment.kt
+++ VoyageVault/app/src/main/java/vault/voyage/app/panelFragments/nearmeFragment/NearMeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import vault.voyage.app.R
 import vault.voyage.app.model.User
 
@@ -17,6 +19,15 @@ class NearMeFragment(val user: User) : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_near_me, container, false)
+        val view = inflater.inflate(R.layout.fragment_near_me, container, false)
+        //Reference: VectorStock.com
+        val background = view.findViewById<ImageView>(R.id.near_me_bg)
+        background.setImageResource(R.drawable.near_me_bg)
+        val nearMeIcon = view.findViewById<ImageView>(R.id.near_me_icon)
+        nearMeIcon.setImageResource(R.drawable.near_me_icon)
+        val nearMeRecyclerview = view.findViewById<RecyclerView>(R.id.near_me_recyclerview)
+
+
+        return view
     }
 }
